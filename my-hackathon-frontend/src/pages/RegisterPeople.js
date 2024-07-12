7038425a688a1c9394e0e69ca4227504ee8e7d72
Pierre Wizla <pwizla+github@gmail.com>
2024-07-12 17:38:46 +0200

    Fix time zones

--- my-hackathon-frontend/src/pages/RegisterPeople.js
+++ my-hackathon-frontend/src/pages/RegisterPeople.js
@@ -97,7 +97,30 @@ const Register = () => {
               <option value="UTC-12">UTC-12</option>
               <option value="UTC-11">UTC-11</option>
               <option value="UTC-10">UTC-10</option>
-              {/* Add other time zones as needed */}
+              <option value="UTC-10">UTC-9</option>
+              <option value="UTC-10">UTC-8</option>
+              <option value="UTC-10">UTC-7</option>
+              <option value="UTC-10">UTC-6</option>
+              <option value="UTC-10">UTC-5</option>
+              <option value="UTC-10">UTC-4</option>
+              <option value="UTC-10">UTC-3</option>
+              <option value="UTC-10">UTC-2</option>
+              <option value="UTC-10">UTC-1</option>
+              <option value="UTC-10">UTC</option>
+              <option value="UTC-10">UTC+1</option>
+              <option value="UTC-10">UTC+1</option>
+              <option value="UTC-10">UTC+2</option>
+              <option value="UTC-10">UTC+3</option>
+              <option value="UTC-10">UTC+4</option>
+              <option value="UTC-10">UTC+5</option>
+              <option value="UTC-10">UTC+6</option>
+              <option value="UTC-10">UTC+7</option>
+              <option value="UTC-10">UTC+8</option>
+              <option value="UTC-10">UTC+9</option>
+              <option value="UTC-10">UTC+10</option>
+              <option value="UTC-10">UTC+11</option>
+              <option value="UTC-10">UTC+11</option>
+              <option value="UTC-10">UTC+12</option>
             </select>
           </label>
           <button type="submit">Continue</button>
