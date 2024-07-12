6626a4e3917850efe0f8055b297e2a18af4d4c44
Pierre Wizla <pwizla+github@gmail.com>
2024-07-12 16:34:20 +0200

    Add logo to register success page

--- my-hackathon-frontend/src/pages/RegisterSuccess.js
+++ my-hackathon-frontend/src/pages/RegisterSuccess.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../RegisterSuccess.css';
+import logo from '../logo.svg';
 
 const Confirmation = () => {
   return (
     <div className="container">
+      <header className="register-header">
+        <a href="/"><img className="logo" src={logo} alt="logo" /></a>
+      </header>
       <div className="confirmation-container">
         <h1>Thank you!</h1>
         <p>We received your registration form. We will inform you when Hackathon will start.</p>
